bfff749fa1f1d9fc2b7eed2074d34920493a4f1d
deepthi7143 <150417779+deepthi7143@users.noreply.github.com>
2024-02-05 10:06:37 +0530

    Update main.tf

--- project-terraform-devops-tools-install/main.tf
+++ project-terraform-devops-tools-install/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami                    = "ami-0277155c3f0ab2930"      #change ami id for different region
   instance_type          = "t2.large"
-  key_name               = "vscode"              #change key name as per your setup
-  vpc_security_group_ids = [aws_security_group.devops-project-veera.id]
+  key_name               = "jenkins"              #change key name as per your setup
+  vpc_security_group_ids = [aws_security_group.sg-0554adcfef2efbe4b.id]
   user_data              = templatefile("./install.sh", {})
 
   tags = {
@@ -14,8 +14,8 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_security_group" "devops-project-veera" {
-  name        = "devops-project-veera"
+resource "aws_security_group" "sg-0554adcfef2efbe4b" {
+  name        = "devops-project-sg"
   description = "Allow TLS inbound traffic"
 
   ingress = [
